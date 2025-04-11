79ed584d4783624333f0d3d55dbcbe1ae54a750e
alok-kumar21 <alokprajapati021@gmail.com>
2025-04-11 22:05:47 +0530

    blog website commit

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="bg-dark text-light py-2">
-    <p className="container">&copy; 2024. My Blogging Website.</p>
-  </footer>
-);
-
-export default Footer;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,52 +1,45 @@
-import 'react-bootstrap';
-import Header from '../components/Header';
-import Footer from "../components/Footer"
 function Blog() {
   return (
-    <>
-      <Header />
-      <main className="container py-4">
-        <h1>Blogs</h1>
-        <div>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="card mt-3">
-                <div className="card-body">
-                  <h5>Blog 1</h5>
-                  <p className="card-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Facere provident culpa sint sequi.
-                  </p>
-                </div>
+    <main className="container py-4">
+      <h1>Blogs</h1>
+      <div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card mt-3">
+              <div className="card-body">
+                <h5>Blog 1</h5>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facere provident culpa sint sequi.
+                </p>
               </div>
             </div>
-            <div className="col-md-6">
-              <div className="card mt-3">
-                <div className="card-body">
-                  <h5>Blog 2</h5>
-                  <p className="card-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Facere provident culpa sint sequi.
-                  </p>
-                </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card mt-3">
+              <div className="card-body">
+                <h5>Blog 2</h5>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facere provident culpa sint sequi.
+                </p>
               </div>
             </div>
-            <div className="col-md-6">
-              <div className="card mt-3">
-                <div className="card-body">
-                  <h5>Blog 3</h5>
-                  <p className="card-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Facere provident culpa sint sequi.
-                  </p>
-                </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card mt-3">
+              <div className="card-body">
+                <h5>Blog 3</h5>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facere provident culpa sint sequi.
+                </p>
               </div>
             </div>
           </div>
         </div>
-      </main>
-      <Footer/>
-    </>
+      </div>
+    </main>
   );
 }
 
